f3d303d40765c886c686ccc80eb7c05a2935bef1
Hariprasath <balaprasad24@gmail.com>
2020-03-08 00:24:24 +0530

    Added basic Hystrix Dashboard for movie catalog service

--- movie-catalog-service-api/src/main/java/in/hp/boot/MovieCatalogServiceApiApplication.java
+++ movie-catalog-service-api/src/main/java/in/hp/boot/MovieCatalogServiceApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -21,9 +22,9 @@ import org.springframework.web.reactive.function.client.WebClient;
  *
  */
 @SpringBootApplication
-//@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogServiceApiApplication {
 
 	/**
